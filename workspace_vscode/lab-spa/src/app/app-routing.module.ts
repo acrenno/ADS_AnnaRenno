e916a0678d6ed642a80a53e0b118669dc8d5c67d
Anna Rennó <110424006+acrenno@users.noreply.github.com>
2023-12-13 15:31:55 -0300

    workspace_vscode

---
+++ workspace_vscode/lab-spa/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookTableComponent } from './components/book-table/book-table.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+
+  {
+    path: 'booktable',
+    component: BookTableComponent
+  },
+
+  {
+    path: '',
+    component: HomeComponent
+
+  },
+
+  {
+    path: '**',
+    component:NotFoundComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ workspace_vscode/lab-spa/src/app/components/book-table/book-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-table',
+  templateUrl: './book-table.component.html',
+  styleUrls: ['./book-table.component.css']
+})
+export class BookTableComponent {
+
+  constructor( private service: BookService ) {}
+
+}
